dd1a9b17cda4c3d52e46328bff7f50221d8710e0
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2025-05-09 22:10:26 +0300

    title

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -44,6 +44,7 @@ export default function HomePage() {
           </ul>
         </nav>
       </div>
+      {/* <hr /> */}
       <h1>The road to the depths of the human soul</h1>
       <p>
         We help you to reveal your potential, overcome challenges and find a
