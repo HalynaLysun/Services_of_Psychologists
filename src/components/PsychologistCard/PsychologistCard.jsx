3b35d0dd1ff2dbb0ab74c53433aac757aa7fa7c3
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2025-05-18 16:34:51 +0300

    firebase

--- src/components/PsychologistCard/PsychologistCard.jsx
+++ src/components/PsychologistCard/PsychologistCard.jsx
@@ -1,3 +1,10 @@
+import css from "./PsychologistCard.module.css";
+import { FaStar } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart } from "react-icons/ai";
+
+import { useState } from "react";
+
 export default function PsychologistCard({
   psychologist: {
     name,
@@ -12,11 +19,51 @@ export default function PsychologistCard({
     about,
   },
 }) {
-  console.log(name);
+  const [isFavorite, setIsFavorite] = useState(false);
+
   return (
-    <>
-      <img src={avatar_url} alt="" />
-      <h1>Name: {name}</h1>
-    </>
+    <div className={css.card}>
+      <div className={css.header}>
+        <div className={css.avatarWrap}>
+          <img className={css.avatar} src={avatar_url} alt={name} />
+          <span className={css.onlineDot}></span>
+        </div>
+
+        <div className={css.topInfo}>
+          <p className={css.role}>Psychologist</p>
+          <h2 className={css.name}>{name}</h2>
+
+          <div className={css.tags}>
+            <span className={css.tag}>
+              Experience: <strong>{experience}</strong>
+            </span>
+            <span className={css.tag}>
+              License: <strong>{license}</strong>
+            </span>
+          </div>
+          <div className={css.tags}>
+            <span className={css.tag}>
+              Specialization: <strong>{specialization}</strong>
+            </span>
+            <span className={css.tag}>
+              Initial_consultation: <strong>{initial_consultation}</strong>
+            </span>
+          </div>
+        </div>
+
+        <div className={css.ratingBlock}>
+          <FaStar color="gold" /> Rating: <strong>{rating}</strong> | Price / 1
+          hour: <span className={css.price}>{price_per_hour}$</span>
+          <button className={css.heart}>
+            {isFavorite ? <AiFillHeart color="black" /> : <AiOutlineHeart />}
+          </button>
+        </div>
+      </div>
+
+      <p className={css.about}>{about}</p>
+      <a href="#" className={css.readMore}>
+        Read more
+      </a>
+    </div>
   );
 }
